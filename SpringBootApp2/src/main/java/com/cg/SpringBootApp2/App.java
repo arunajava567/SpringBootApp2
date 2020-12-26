7a4cb08eabc52a075f303b34cc8cbebbb262d011
arunajava567 <arunajava567@example.com>
2020-12-26 17:32:01 +0530

    comment1

--- SpringBootApp2/src/main/java/com/cg/SpringBootApp2/App.java
+++ SpringBootApp2/src/main/java/com/cg/SpringBootApp2/App.java
@@ -12,7 +12,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!rwerwerwerwerewrdfdsfsdfdsfds" );
+        System.out.println( "Hello World!" );
+        System.out.println("Spring Boot Practice example ");
         SpringApplication.run(App.class, args);
     }
 }
